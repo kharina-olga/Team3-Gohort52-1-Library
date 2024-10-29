04349ec188ab24a2f7022044680220efd5605c87
Olga Kharina <oskharina@gmail.com>
2024-10-29 22:24:07 +0100

    partially implemented UserRepository, UserRepository.impl, BookRepository, BookRepositoryImpl

--- src/repository/BookRepositoryImpl.java
+++ src/repository/BookRepositoryImpl.java
@@ -1,7 +1,33 @@
 package repository;
 
-public class BookRepositoryImpl implements{
+import model.Book;
+import utils.MyList;
+
+public class BookRepositoryImpl implements BookRepository {
 
 
+    @Override
+    public void addBook(int id, String title, String author) {
+
+    }
+
+    @Override
+    public void getBookById(int id) {
+
+    }
+
+    @Override
+    public MyList<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getAvailableBooks() {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getBorrowedBooks() {
+        return null;
+    }
 }
-п
