3feeb999b28bea43c8d8d6e1d57f4aeaa750b69d
IgorDerk <derk.i26252@gmail.com>
2024-10-29 12:21:58 +0100

    add class Book

---
+++ src/model/Book.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public  void returnBook(){
+        isAvailable = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
